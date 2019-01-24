7c6cb09c972c1ff3c918eccf56fd6bbd21414b5b
Your Name <you@example.com>
2019-01-24 21:09:49 +0100

    clase objeto creada

--- Reto3/src/reto3/modelo/cliente.java
+++ Reto3/src/reto3/modelo/cliente.java
@@ -5,10 +5,19 @@
  */
 package reto3.modelo;
 
-/**
- *
- * @author Tarde
- */
+import java.util.Date;
+
+
 public class cliente {
+    public String DNI,nombre,apellidos,contraseña;
+    public Date fecha_nac;
     
+    public void cliente(String DNI,String nombre,String apellidos,String contraseña,Date fecha_nac)
+    {
+    this.DNI=DNI;
+    this.nombre=nombre;
+    this.apellidos=apellidos;
+    this.contraseña=contraseña;
+    this.fecha_nac=fecha_nac;
+    }
 }
