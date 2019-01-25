97734013d8d6b1f366dd3f238465c876e6e8d971
MaiGarxi <mai_garxi@hotmail.com>
2019-01-25 16:24:46 +0100

    borrar codigo inutil

--- Reto3/src/reto3/modelo/lineas.java
+++ Reto3/src/reto3/modelo/lineas.java
@@ -3,14 +3,11 @@ package reto3.modelo;
 
 public class lineas {
     
-	public String Cod_Linea, Cod_Parada, Cod_Bus;
+	public String Cod_Linea, Cod_Parada;
 	public String nombre;
 	
 	public lineas(String Cod_Linea, String nombre) {
 		this.Cod_Linea = Cod_Linea;
 		this.nombre = nombre;
-    }
-
-
-        
+    }        
 }
