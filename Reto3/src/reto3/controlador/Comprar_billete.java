5d2bbcf1caeaba0abfd8b9b29c2a0f43f8c77a2d
dimaggio <noesvenezuela@gmail.com>
2019-01-27 23:26:18 +0100

    cliente por todos lados

---
+++ Reto3/src/reto3/controlador/Comprar_billete.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package reto3.controlador;
+
+import reto3.modelo.cliente;
+
+/**
+ *
+ * @author RoboCop
+ */
+public class Comprar_billete {
+    public Comprar_billete(cliente cliente){
+    reto3.controlador.pasar_pagina.paradas_a_lista(cliente);
+    
+}
+    
+}
